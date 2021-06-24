9a13d912828a9d83e450e89f085ad717418091d2
Austin F. Oltmanns <aoltmanns@mines.edu>
2021-06-24 15:58:36 -0600

    added hysteresis plot

--- code/load_data_and_plot_june17.py
+++ code/load_data_and_plot_june17.py
@@ -2,11 +2,20 @@ import loaders
 import conversions
 
 import matplotlib.pyplot as plt
+import matplotlib # checked for version
 import time
 import pdb
 from scipy import signal
 import numpy as np
 
+from scipy.interpolate import interp1d
+
+# Set figures
+fontsize = 18
+plt.rc('font', size=fontsize, family='serif')
+plt.rc('axes', titlesize=fontsize)
+plt.rc('axes', labelsize=fontsize)
+plt.rc('legend', fontsize=fontsize)
 
 # load data
 samples = [1,2,3]
@@ -92,42 +101,70 @@ for sample in samples:
   freq_meas[sample] = freq_sample
   cap_meas[sample]  = cap_sample
 
+
+# Generate Plots
+
 # plot cap v time and force v time
 print("Plotting Data...")
+print('matplotlib: {}'. format(matplotlib. __version__))
 #pdb.set_trace()
 this_time = time.time()
-plt.figure()
+
+rate_colors = {2: (0.2, 0.3, 0.8), 4: (0.4, 0.7, 0.6),
+               6: (0.6, 0.3, 0.4), 8: (0.8, 0.7, 0.2), 10: (1.0, 0.3, 0.0)}
+
+fig, (ax1, ax2) = plt.subplots(2,1,sharex=True)
 for sample in samples:
-  plt.subplot(2,1,1)
   for rate in rates:
     forces = [-point["kN"] for point in force_data[sample][rate]]
     times  = [point["Sec"] for point in force_data[sample][rate]]
-    plt.plot(times[::100],forces[::100])
-  plt.legend(["{0} kN/s".format(rate) for rate in rates])
-  plt.ylabel("Force (kN)")
-  plt.xlabel("Time (s)")
-  plt.title("Applied Force vs Time")
-  plt.subplot(2,1,2)
-#  for rate in rates:
+    ax1.plot(times[::100],forces[::100], color=rate_colors[rate])
+  ax1.legend(["{0} kN/s".format(rate) for rate in rates])
+  ax1.set_ylabel("Force (kN)")
+  #plt.xlabel("Time (s)")
+  ax1.set_title("Applied Force vs Time")
+#  for rate in rates: # (dont) plot cap raw data
 #    caps = [point["chan_c"] for point in cap_data[sample][rate]]
 #    plt.plot(caps[::100])
-  for rate in rates:
+  rate_plot_handle = []
+  for rate in rates: # plot median filtered data
     pf_cap = [c/1.0e-12 for c in cap_meas[sample][rate][15:-15:100]] 
     times  = [point["Sec"] for point in cap_data[sample][rate][15:-15:100]]
-    plt.plot(times, pf_cap)
-  plt.legend(["{0} kN/s filtered".format(rate) for rate in rates])
-  plt.ylabel("Capacitance (pF)")
-  plt.xlabel("Time (s)")
-  plt.title("Measured Cap. vs Time")
+    rate_plot_handle_, = ax2.plot(times, pf_cap, 
+                              label="{0} kN/s".format(rate), color=rate_colors[rate])
+    rate_plot_handle.append(rate_plot_handle_)
+
+  # Split legend
+  first_legend = ax2.legend(handles=rate_plot_handle[:2], loc='lower center')
+  # Add the legend manually to the current Axes.
+  plt.gca().add_artist(first_legend)
+  # Create another legend for the second set
+  ax2.legend(handles=rate_plot_handle[2:], loc='lower right')
+
+  ax2.set_ylabel("Capacitance (pF)")
+  ax2.set_xlabel("Time (s)")
+  ax2.set_title("Measured Cap. vs Time")
 #  plt.ylim([2500,2700])
 
+# plot force v cap
+# interpolate force
+fig2 = plt.figure()
+for sample in samples:
+  for rate in rates:
+    forces = [-point["kN"] for point in force_data[sample][rate]]
+    force_times = [point["Sec"] for point in force_data[sample][rate]]
+    interp_func = interp1d(force_times, forces,  bounds_error=False, fill_value=0.0)
+    pf_cap = [c/1.0e-12 for c in cap_meas[sample][rate][15:-15:100]] 
+    cap_times  = [point["Sec"] for point in cap_data[sample][rate][15:-15:100]]
+    interp_forces = interp_func(cap_times)
+    plt.plot(pf_cap, interp_forces, color=rate_colors[rate])
+plt.legend(["{0} kN/s".format(rate) for rate in rates])
+plt.xlabel('Capacitance (pF)')
+plt.ylabel("Force (kN)")
+plt.title("Force vs Capacitance")
 
 that_time = time.time()
 print("Data plotted in {0} sec".format(that_time - this_time))
-plt.rc('font', size=22)
-plt.rc('axes', titlesize=22)
-plt.rc('axes', labelsize=22)
-plt.rc('legend', fontsize=22)
 plt.show(block=False)
 input("Press Enter to close.")
 # plot force v cap
