58e1e44b8b1df376a9f280b74b326917e900a90f
Austin F. Oltmanns <aoltmanns@mines.edu>
2021-08-22 16:13:46 -0600

    registered signals and set figures

--- code/load_data_and_plot_limestone_summer_21.py
+++ code/load_data_and_plot_limestone_summer_21.py
@@ -77,30 +77,33 @@ lcm_data_files = {"New": {"0.1 in.": { 0: base_path + "lcm_data/New 0.1inch/line
                                       16: base_path + "lcm_data/New 0.1inch/line_16.lvm",
                                       17: base_path + "lcm_data/New 0.1inch/line_17 (edge).lvm"}},
                 "Mod.": {"0.1 in.": {  0: base_path + "lcm_data/MOD 0.1inch/line_0 (edge).lvm",
-                                       1: base_path + "lcm_data/New 0.1inch/line_1.lvm",
-                                       2: base_path + "lcm_data/New 0.1inch/line_2.lvm",
-                                       3: base_path + "lcm_data/New 0.1inch/line_3.lvm",
-                                       4: base_path + "lcm_data/New 0.1inch/line_4.lvm",
-                                       5: base_path + "lcm_data/New 0.1inch/line_5.lvm",
-                                       6: base_path + "lcm_data/New 0.1inch/line_6.lvm",
-                                       7: base_path + "lcm_data/New 0.1inch/line_7.lvm",
-                                       8: base_path + "lcm_data/New 0.1inch/line_8.lvm",
-                                       9: base_path + "lcm_data/New 0.1inch/line_9.lvm",
-                                       10: base_path + "lcm_data/New 0.1inch/line_10.lvm",
-                                       11: base_path + "lcm_data/New 0.1inch/line_11.lvm",
-                                       12: base_path + "lcm_data/New 0.1inch/line_12.lvm",
-                                       13: base_path + "lcm_data/New 0.1inch/line_13.lvm",
-                                       14: base_path + "lcm_data/New 0.1inch/line_14.lvm",
-                                       15: base_path + "lcm_data/New 0.1inch/line_15.lvm",
-                                       16: base_path + "lcm_data/New 0.1inch/line_16.lvm",
-                                       17: base_path + "lcm_data/New 0.1inch/line_17 (edge).lvm"}}}
+                                       1: base_path + "lcm_data/MOD 0.1inch/line_1.lvm",
+                                       2: base_path + "lcm_data/MOD 0.1inch/line_2.lvm",
+                                       3: base_path + "lcm_data/MOD 0.1inch/line_3.lvm",
+                                       4: base_path + "lcm_data/MOD 0.1inch/line_4.lvm",
+                                       5: base_path + "lcm_data/MOD 0.1inch/line_5.lvm",
+                                       6: base_path + "lcm_data/MOD 0.1inch/line_6.lvm",
+                                       7: base_path + "lcm_data/MOD 0.1inch/line_7.lvm",
+                                       8: base_path + "lcm_data/MOD 0.1inch/line_8.lvm",
+                                       9: base_path + "lcm_data/MOD 0.1inch/line_9.lvm",
+                                       10: base_path + "lcm_data/MOD 0.1inch/line_10.lvm",
+                                       11: base_path + "lcm_data/MOD 0.1inch/line_11.lvm",
+                                       12: base_path + "lcm_data/MOD 0.1inch/line_12.lvm",
+                                       13: base_path + "lcm_data/MOD 0.1inch/line_13.lvm",
+                                       14: base_path + "lcm_data/MOD 0.1inch/line_14.lvm",
+                                       15: base_path + "lcm_data/MOD 0.1inch/line_15.lvm",
+                                       16: base_path + "lcm_data/MOD 0.1inch/line_16.lvm",
+                                       17: base_path + "lcm_data/MOD 0.1inch/line_17 (edge).lvm"}}}
 #pdb.set_trace()
 print("Loading Data...")
 this_time = time.time()
 cap_data = {}
 lcm_data = {}
-cap_dt = 1.0/500.0
-cap_t_off = 2.0
+cap_dt = 0.0025
+
+force_time_offsets = {wear_levels[0]: 1.34, wear_levels[1]: 1.3}
+cap_time_offsets =  {wear_levels[0]: 4.65, wear_levels[1]: 7.43}
+plot_duration = 5.5 # seconds
 
 for wear in wear_levels:
   cap_data[wear] = {}
@@ -129,7 +132,7 @@ print("Data loaded in {0} sec".format(that_time - this_time))
 print("Plotting Data...")
 # Plot all wear levels in same plot for one representative sample
 this_time = time.time()
-fig, (ax1, ax2) = plt.subplots(2,1,sharex=False)
+fig, (ax1, ax2) = plt.subplots(2,1,sharex=True)
 
 rep_pen = penetrations[0]
 rep_line = 1
@@ -139,26 +142,42 @@ wear_colors = {wear_levels[0]: (0.2, 0.3, 0.8),
                #wear_levels[2]: (1.0, 0.3, 0.0)
                }
 
+def first_index_greater_than(input_iterable, item):
+  try:
+    res = next(x for x, val in enumerate(input_iterable) if val > item)
+  except StopIteration:
+    res = len(input_iterable)
+  return res
+
 for wear in wear_levels:
   force_values = [conversions.calculate_drag_force(
                     point["v1"], point["v2"], point["v3"], point["v4"])
                   for point in lcm_data[wear][rep_pen][rep_line]]
-  force_times = [point["Sec"] for point in lcm_data[wear][rep_pen][rep_line]]
-  ax1.plot(force_times, force_values, color=wear_colors[wear])
+  force_times = [point["Sec"]-force_time_offsets[wear] 
+                  for point in lcm_data[wear][rep_pen][rep_line]]
+  plot_start = first_index_greater_than(force_times, 0.0)
+  plot_end   = first_index_greater_than(force_times, plot_duration)
+  ax1.plot(force_times[plot_start:plot_end], force_values[plot_start:plot_end],
+           color=wear_colors[wear])
 ax1.legend(["{0}".format(wear) for wear in wear_levels])
 ax1.set_ylabel("Force (lbf)")
-ax1.set_xlabel("Time (s)")
-ax1.set_title("Applied Force vs Time")
+#ax1.set_xlabel("Time (s)")
+ax1.set_title("Applied Force vs Time; 0.1 in. pen.")
 
 for wear in wear_levels:
-  cap_values  = [point["chan_b"] for point in cap_data[wear][rep_pen][rep_line]]
-  cap_times   = [point["Sec"] for point in cap_data[wear][rep_pen][rep_line]]
-  ax2.plot(cap_times, cap_values, color=wear_colors[wear])
+  cap_values  = [point["chan_b"] 
+                  for point in cap_data[wear][rep_pen][rep_line]]
+  cap_times   = [point["Sec"]-cap_time_offsets[wear] 
+                  for point in cap_data[wear][rep_pen][rep_line]]
+  plot_start = first_index_greater_than(cap_times, 0.0)
+  plot_end   = first_index_greater_than(cap_times, plot_duration)
+  ax2.plot(cap_times[plot_start:plot_end], cap_values[plot_start:plot_end],
+           color=wear_colors[wear])
 ax2.legend(["{0}".format(wear) for wear in wear_levels])
 ax2.set_ylabel("Capacitance (pF)")
 ax2.set_xlabel("Time (s)")
 ax2.set_title("Measured Cap. vs Time")
-ax2.set_ylim([500, 800])
+ax2.set_ylim([520, 580])
 #force_values = [conversions.calculate_drag_force(point["v1"], point["v2"], point["v3"], point["v4"])
 #                for point in lcm_data["Mod."]["0.1 in."][3]]
 #force_times  = [point["Sec"] for point in lcm_data["Mod."]["0.1 in."][3]]
