5b3d529780f3223b8e280159c72ec296a30703ee
Austin F. Oltmanns <aoltmanns@mines.edu>
2023-04-01 15:50:25 -0600

    added data and updated conversions

--- code/conversions.py
+++ code/conversions.py
@@ -1,6 +1,6 @@
 from math import pi
 
-def calculate_freq_and_cap(meas, L, Cfilt, gain=4.0): # gain is no. of bits shifted (?)
+def calculate_freq_and_cap(meas, L, Cfilt, gain=4.0): # gain is no. of bits shifted
   freqs = [40.0e6 * mea / (2**(12+gain)) for mea in meas]
   caps  = [1.0 / (L*(2.0*pi*freq)**2) - Cfilt for freq in freqs]
   return freqs, caps
